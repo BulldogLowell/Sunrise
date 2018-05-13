5b41545b2e43c82c1cb53666799fae4138c897a4
Jim Brower <browerjames@gmail.com>
2018-05-13 14:50:25 -0400

    Update exhaustiveExample.ino
    updated description

--- firmware/examples/exhaustiveExample.ino
+++ firmware/examples/exhaustiveExample.ino
@@ -1,23 +1,24 @@
+
 /*
   Sunrise Library
   Adapted for Particle Devices May - 2018
+  Copyright May, 2018
   Jim Brower
   bulloglowell@gmail.com
-
   Twilight definitions:
   ACTUAL is the moment the Sun sets completely below the horizon
   CIVIL is the moment that the center of the sun is 6° below the horizon
   NAUTICAL is the moment that the center of the sun is 12° below the horizon
   ASTRONOMICAL is the moment that the center of the sun is 18° below the horizon
-
   for more on CIVIL, NAUTICAL and ASTRONOMICAL twilight, see the following:
   https://en.wikipedia.org/wiki/Twilight
-
   check your times here:
+  https://sunrise-sunset.org
+  or as this example:
   https://sunrise-sunset.org/us/princeton-nj
-
-  Derived From:
+  Originally Derived From:
   http://www.swfltek.com/arduino/sunrise
+  (but seems to now be defunct...)
 */
 
 #include "Sunrise.h"
